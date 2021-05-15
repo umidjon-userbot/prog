83ec5aa6cc9f5e5c2fc6f14e64cdf48c105fa7c1
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2021-05-15 18:58:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def leavevc(_, message):
     )
  
          #---------------------#
-@app.on_message(filters.regex("joinvc")  & ~filters.private
+@app.on_message(filters.regex("joinvc")  & filters.user(SUDOERS) & ~filters.private
 )
 async def joinvc(_, message):
     global call
