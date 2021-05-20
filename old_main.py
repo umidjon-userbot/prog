587f16adec291c1f8cae9e251b77d1213ec4046c
umidjon-userbot <62179002+umidjon-userbot@users.noreply.github.com>
2021-05-20 07:14:19 +0500

    Update old_main.py

--- old_main.py
+++ old_main.py
@@ -802,7 +802,27 @@ async def quotly_func(_, message: Message):
         await m.delete()
         return         
       #--------------------------------------------------------------------------#
-      
+@app.on_message(filters.command("screen"))
+#@capture_err
+async def take_ss(_, message):
+    try:
+        if len(message.command) != 2:
+            await message.reply_text("Give A Url To Fetch Screenshot.")
+            return
+        url = message.text.split(None, 1)[1]
+        m = await message.reply_text("**Taking Screenshot**")
+        await m.edit("**Uploading**")
+        try:
+            await app.send_photo(
+                message.chat.id,
+                photo=f"https://webshot.amanoteam.com/print?q={url}",
+            )
+        except TypeError:
+            await m.edit("No Such Website.")
+            return
+        await m.delete()
+    except Exception as e:
+        await message.reply_text(str(e))      
       
       
       
